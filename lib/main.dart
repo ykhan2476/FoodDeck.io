bf5f054e47594b06ed256e246a4ed2337f87d04d
yasha khan <113904335+ykhan2476@users.noreply.github.com>
2024-03-29 13:25:22 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,7 @@ import 'package:fooddeck/screens/splash.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
- /* await Firebase.initializeApp(options: FirebaseOptions(
-    apiKey:"AIzaSyBv0P3txd_Q3xpi7TEIW-iCXi9CoPFvKM0" ,
-     appId: "1:624817020162:android:f3b211de9654b6aaafb61b",
-      messagingSenderId:"624817020162" , 
-      projectId: "fees-ce3dd"));*/
+ 
   runApp(const MyApp());
 }
 
